8fef4d14045de6654449ceee4384e5448021a5ea
Francesco Sacco <francescosacco.github@gmail.com>
2026-03-23 12:53:11 +0100

    feat: check if current directory contains .git repo

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
 
 func main() {
     fmt.Println("Hello Gitsloth in Go!")
+}
+
+func isGitRepoHere() bool {
+    cwd, err := os.Getwd()
+    if err != nil {
+        return false
+    }
+    gitPath := filepath.Join(cwd, ".git")
+    info, err := os.Stat(gitPath)
+    return err == nil && info != nil
 }
