4997268a0915410f735ab755c7fec2cd46c9dcc8
Francesco Sacco <francescosacco.github@gmail.com>
2026-03-23 13:09:33 +0100

    feat: add Git diff retrieval and printing if changes exist

--- main.go
+++ main.go
@@ -3,19 +3,46 @@ package main
 import (
 	"fmt"
 	"os"
+	"os/exec"
 	"path/filepath"
 )
 
 func main() {
-    fmt.Println("Hello Gitsloth in Go!")
+	if !isGitRepoHere() {
+		fmt.Println("Not inside a Git repository (.git not found here)")
+		os.Exit(1)
+	}
+	diff, err := getGitDiff()
+	if err != nil {
+		fmt.Println("Failed to get `git diff`", err)
+		os.Exit(1)
+	}
+	if diff == "" {
+		fmt.Println("No changes to commit")
+	} else {
+		fmt.Println("Git diff:")
+		fmt.Println(diff)
+	}
 }
 
 func isGitRepoHere() bool {
-    cwd, err := os.Getwd()
-    if err != nil {
-        return false
-    }
-    gitPath := filepath.Join(cwd, ".git")
-    info, err := os.Stat(gitPath)
-    return err == nil && info != nil
+	var cwd string
+	var err error
+	cwd, err = os.Getwd()
+	if err != nil {
+		return false
+	}
+	var gitPath string = filepath.Join(cwd, ".git")
+	var info os.FileInfo
+	info, err = os.Stat(gitPath)
+	return err == nil && info != nil
+}
+
+func getGitDiff() (string, error) {
+	cmd := exec.Command("git", "diff", "--cached")
+	output, err := cmd.Output()
+	if err != nil {
+		return "", err
+	}
+	return string(output), nil
 }
